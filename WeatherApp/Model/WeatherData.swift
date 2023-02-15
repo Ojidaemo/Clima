4d19aca4abcf7fc3b02f9a5659bd5c5e71cca108
Ojidaemo <vit.martsinovich@gmail.com>
2023-02-15 15:43:38 +0300

    Complete app

---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by Vitali Martsinovich on 2023-02-14.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
